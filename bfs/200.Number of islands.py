2df94f8a8117482a346bed07b00987e406b7762b
Amelia Liang <dliang0504@utexas.edu>
2025-06-13 00:00:31 -0500

    added 1 problem

---
+++ bfs/200.Number of islands.py
@@ -0,0 +1,27 @@
+from typing import List
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = [[False] * cols for _ in range(rows)]
+
+        def bfs(currRow, currCol):
+            queue = deque([(currRow, currCol)]) # row, col
+            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            while queue:
+                currRow, currCol = queue.popleft()
+                for dr, dc in directions:
+                    neiRow, neiCol = currRow + dr, currCol + dc
+                    if(0 <= neiRow < rows and 0 <= neiCol < cols and grid[neiRow][neiCol] == "1" and not visited[neiRow][neiCol]):
+                        visited[neiRow][neiCol] = True
+                        queue.append((neiRow, neiCol))
+        island = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1" and not visited[i][j]:
+                    bfs(i, j)
+                    island += 1
+        return island
+
+
+
