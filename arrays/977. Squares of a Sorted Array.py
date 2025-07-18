db0f0560340ba095a5b305ae0f6e3a07ee01d48f
Amelia Liang <dliang0504@utexas.edu>
2025-07-17 22:32:20 -0500

    Add a problem

---
+++ arrays/977. Squares of a Sorted Array.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        result = [0] * (len(nums))
+        # fill from the end
+        currPos = len(nums) - 1
+        # two pointer
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            if abs(nums[left]) <= abs(nums[right]):
+                result[currPos] = nums[right] ** 2
+                right -= 1
+            else:
+                result[currPos] = nums[left] ** 2
+                left += 1
+            currPos -= 1
+        return result
